e0082af0626b8790b02bee27d9e5df1a4c7e8863
Dennis Anane Amematekpor <dennisamematekpor@gmail.com>
2023-10-17 21:46:48 +0000

    added FlatList

--- src/features/components/restaurant-info-card.component.js
+++ src/features/components/restaurant-info-card.component.js
@@ -24,6 +24,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
+
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily = true,
