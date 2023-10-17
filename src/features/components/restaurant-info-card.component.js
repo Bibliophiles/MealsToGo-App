57721ab4af87011942740b15dbcab6cbd84ca093
Dennis Anane Amematekpor <dennisamematekpor@gmail.com>
2023-10-17 16:01:00 +0000

    Added typography

--- src/features/components/restaurant-info-card.component.js
+++ src/features/components/restaurant-info-card.component.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
-import { Text, Image } from "react-native";
 import { SvgXml } from "react-native-svg";
 import { Spacer } from "../../components/spacer/spacer.component";
+import { Text } from "../../components/typography/text.component";
 import star from "../../../assets/star";
 import open from "../../../assets/open";
 
@@ -16,12 +16,6 @@ const RestaurantCardCover = styled(Card.Cover)`
   color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
 const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
@@ -32,9 +26,9 @@ const Rating = styled.View`
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
+const Icon = styled.Image`
+  height: 15px;
+  width: 15px;
 `;
 
 const Section = styled.View`
@@ -66,7 +60,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map(() => (
@@ -75,20 +69,17 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && (
-              <Text variant="label" style={{ color: "red" }}>
-                CLOSED TEMPORARILY
-              </Text>
+              <Text variant="error">CLOSED TEMPORARILY</Text>
             )}
-            <Spacer position="left" size="large">
+            <Spacer position="left" size="medium">
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
             </Spacer>
-            <Spacer position="left" size="large">
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            <Spacer position="left" size="medium">
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
-
-        <Address>{address}</Address>
+        <Text vaiant="caption">{address}</Text>
       </Info>
     </RestaurantCard>
   );
