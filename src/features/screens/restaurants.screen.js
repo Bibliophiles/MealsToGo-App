9c3cd9a99748ad8406c41cb1dc65dc24fa810b56
Dennis Anane Amematekpor <dennisamematekpor@gmail.com>
2023-10-18 21:57:37 +0000

    Removed the whitebar for safe area

--- src/features/screens/restaurants.screen.js
+++ src/features/screens/restaurants.screen.js
@@ -6,7 +6,6 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 import { Spacer } from "../../components/spacer/spacer.component";
 
 const SafeArea = styled(SafeAreaView)`
-  flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
