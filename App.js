0f8c8c0a0d00c401f2d52a560e3b6cc4ba36751f
Dennis Anane Amematekpor <dennisamematekpor@gmail.com>
2023-10-18 23:04:20 +0000

    added react navigation without header shown

--- App.js
+++ App.js
@@ -6,8 +6,15 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/screens/restaurants.screen";
+import { MapScreen } from "./src/features/screens/map.screen";
+import { SettingsScreen } from "./src/features/screens/settings.screen";
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -24,7 +31,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />
