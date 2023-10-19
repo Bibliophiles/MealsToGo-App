4dcf82a4e656e90df2a2dd90543972d8cbe86e80
Dennis Anane Amematekpor <dennisamematekpor@gmail.com>
2023-10-19 19:05:30 +0000

    added the activity indicator

--- src/features/screens/restaurants.screen.js
+++ src/features/screens/restaurants.screen.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { View, FlatList } from "react-native";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
+
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../components/spacer/spacer.component";
 import { SafeArea } from "../../components/utility/safe-area.component";
@@ -18,10 +20,25 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
